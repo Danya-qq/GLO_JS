e0d5b81fe1a36950c466dd4c4aaaf3f8c4d2ef8b
Danya-qq <qallandbb@gmail.com>
2020-03-31 09:10:06 +0300

    modified JS code

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
 // 1)
 
-let money = 10e4,
+let money = +prompt('Ваш месячный доход?'),
 income = 'freelance',
-addExpenses = 'интернет, КУ, такси, развлечения',
-deposit = true,
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
 mission = 10e5,
 period = 6,
-budgetDay = money/30;
-
-// // 2)
-
-// // console.log(typeof money);
-// // console.log(typeof income);
-// // console.log(typeof deposit);
-// // console.log(addExpenses.length);
-// // console.log('Период равен ' + period + ' месяцев.' + ' Цель заработать ' + mission + ' рублей');
-
-// // console.log(addExpenses.toLowerCase().split(','));
-
-// // console.log(budgetDay);
-
-
-money = +prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-let expenses1 = prompt('Введите обязательную статью расходов'),
+expenses1 = prompt('Введите обязательную статью расходов'),
 amount1 = +prompt('Во сколько это обойдется?'),
 expenses2 = prompt('Введите обязательную статью расходов'),
-amount2 = +prompt('Во сколько это обойдется?');
-
-let budgetMonth = money - amount1 - amount2;
-console.log('Бюджет на месяц ' + budgetMonth);
-console.log('Цель будет достигнута за: ' + Math.ceil(mission/budgetMonth) + ' месяцев');
+amount2 = +prompt('Во сколько это обойдется?'),
+budgetMonth = money - amount1 - amount2,
 budgetDay = budgetMonth/30;
-console.log('Бюджет на день: ' + Math.floor(budgetDay));
 
 if (budgetDay<0) {
     console.log('Что-то пошло не так');
@@ -45,6 +22,11 @@ console.log('У вас средний уровень дохода');
 } else {
     console.log('У вас высокий уровень дохода');  
 };
+
+console.log('Бюджет на месяц ' + budgetMonth);
+console.log('Цель будет достигнута за: ' + Math.ceil(mission/budgetMonth) + ' месяцев');
+console.log('Бюджет на день: ' + Math.floor(budgetDay));
+
 
 
 
